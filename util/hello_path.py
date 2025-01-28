cc73340165c0ebdd3dc24315aa46caf846f2a473
Dylan Clouden <dclouden22@gmail.com>
2025-01-28 13:54:02 -0500

    Added Response in hello_path and example test for response

--- util/hello_path.py
+++ util/hello_path.py
@@ -1,5 +1,6 @@
-
+from response import Response
 # This path is provided as an example of how to use the router
 def hello_path(request, handler):
-    response = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nhello"
-    handler.request.sendall(response.encode())
+    res = Response()
+    res.text("hello")
+    handler.request.sendall(res.to_data())
